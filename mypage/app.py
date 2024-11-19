b68c9f00b2a6d4c02b2a52f403a163c2db936f5e
Meaning-Mentos <ssyuk1357@gmail.com>
2024-11-19 22:12:43 +0900

    11-19 setting

---
+++ mypage/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# 사용자 정의 필터 생성
+@app.template_filter('number')
+def format_number(value):
+    try:
+        return "{:,}".format(value)
+    except ValueError:
+        return value
+
+@app.route('/')
+def profile():
+    user_data = {
+        "name": "홍길동",
+        "email": "honggildong@syuin.ac.kr",
+        "phone": "010-1234-5678",
+        "points": 100000,  # 숫자 그대로 전달
+    }
+    return render_template('profile.html', user=user_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
