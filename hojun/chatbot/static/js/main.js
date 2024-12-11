a1f06f8d4766a409c1e8a15795910747a04ac7b5
ro-jun <thdghwns1191@naver.com>
2024-12-12 03:32:44 +0900

    feat(chatbot): "초기 메시지 설정 + 데이터 입력 편의성" - chatbot.js : code refactoring - events.js: 초기 메세지 및 초기화, 사용자 입력 Shitf + enter입력시 아랫줄 이동 - main.js(rm): 필요 없는 파일 - chatbot.js: 파일이름 바꿈 - chatbot.html: js명 바뀜으로 인한 변경

--- hojun/chatbot/static/js/main.js
+++
@@ -1,5 +0,0 @@
-import { initializeChatbot, handleSendMessage, handleNewChat } from "./events.js";
-
-initializeChatbot(); // 초기 메시지 로드
-handleSendMessage(); // 메시지 전송 핸들러
-handleNewChat();     // 새 채팅하기 핸들러
