38fd3f2b7aad3ac3b8b96b2d953842cc9e47e100
ro-jun <thdghwns1191@naver.com>
2024-12-11 13:40:07 +0900

    remove: "의미 없는 파일 삭제" - llm_chain_setup.py: 체인 필요없음 - prompts.py - similar_search.py: function.py 파일로 기능을 옮김.

--- hojun/chatbot/llm_chain_setup.py
+++
@@ -1,8 +0,0 @@
-from langchain_core.output_parsers import StrOutputParser
-from hojun.chatbot.llm_setup import llm
-from hojun.chatbot.prompt import prompt
-
-# LangChain 표현식 언어 체인 사용
-llm_chain = prompt | llm | StrOutputParser()
-
-print("LLM 체인 설정 완료.")
